eb7c59f5a21f02a468eaee61f6ebf13f354b7ce3
Richard Duah Boakye <richrichie784@gmail.com>
2023-10-14 21:28:53 +0000

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -5,8 +5,7 @@ magic module which nitializes models package
 """
 
 
-from models.engine.file_storage import FileStorage
+from models.engine import file_storage
 
-
-storage = FileStorage()
+storage = file_storage.FileStorage()
 storage.reload()
