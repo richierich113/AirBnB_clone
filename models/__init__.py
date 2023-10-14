8bfdd182e05afc3db8fe3ef05e5b38d13ca6c56e
Richard Duah Boakye <richrichie784@gmail.com>
2023-10-14 20:36:32 +0000

    Update __init__.py

---
+++ models/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""module create a unique FileStorage instance for
+application
+magic module which nitializes models package
+"""
+
+
+from models.engine import file_storage
+
+
+storage = file_storage.FileStorage()
+storage.reload()
