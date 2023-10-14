8f7e128bae6364cd8787662dec81f03021134188
Richard Duah Boakye <richrichie784@gmail.com>
2023-10-14 11:33:47 +0000

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ module with BaseModel super class which all other class models
-in the project inherits from 
+in the project inherits from
 """
 
 
@@ -19,12 +19,12 @@ class BaseModel:
         which initializes instance attributes passed to the class
         """
         if len(kwargs) >= 1 and kwargs is not None:
-           self.set_kwargs_as_attr(**kwargs)
+            self.set_kwargs_as_attr(**kwargs)
         else:
-           self.id = str(uuid.uuid4())
-           self.created_at = datetime.now()
-           self.updated_at = datetime.now()
-           models.storage.new(self)
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """Returns string in the format
@@ -33,12 +33,13 @@ class BaseModel:
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """updates the public instance attribute updated_at with the current datetime
+        """updates the public instance attribute updated_at with the
+        current datetime
         """
         self.updated_at = datetime.now()
         models.storage.save()
 
-     def to_dict(self):
+    def to_dict(self):
         """returns a dictionary containing all keys/values of __dict__ of the instance
         """
         custom_dict = self.__dict__.copy()
