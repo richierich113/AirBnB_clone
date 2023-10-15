ada6a4ed505a59045a0cd4cd9c6c7b3c1627ff8b
Richard Duah Boakye <richrichie784@gmail.com>
2023-10-15 17:14:40 +0000

    Create console.py

---
+++ console.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""the entry point of the command interpreter:
+"""
+
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """class HBNBCommand
+    """
+    prompt = "(hbnb) "
+
+    def do_quit(self, argum):
+        """This exits the command interpreter"""
+        return True
+
+    def do_EOF(self, argum):
+        """Thi exits the command interpreter when user
+        presses Ctrl+D"""
+        print()
+        return True
+
+    def emptyline(self):
+        """Do nothing when line is empty and ENTER is pressed"""
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
