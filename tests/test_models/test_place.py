de38aeca9abaeff348cf79bf7ecdd2f428cd72bc
Richard Duah Boakye <richrichie784@gmail.com>
2023-10-16 02:55:22 +0000

    Update test_place.py

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -34,5 +34,6 @@ class TestPlace(unittest.TestCase):
             self.assertTrue(hasattr(self.place, attr_name))
             self.assertIsInstance(getattr(self.place, attr_name), attr_type)
 
+
 if __name__ == "__main__":
     unittest.main()
