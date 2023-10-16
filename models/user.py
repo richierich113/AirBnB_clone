c38d14cd93f5fca108da2c3e8e1d64e070b61ddc
Richard Duah Boakye <richrichie784@gmail.com>
2023-10-16 00:45:29 +0000

    Create user.py

---
+++ models/user.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" module creating a User class"""
+
+
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """class User that inherits from BaseModel"""
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
