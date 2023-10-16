9bd0342460e182ae9b637c0e557bfec30f9e42e1
Richard Duah Boakye <richrichie784@gmail.com>
2023-10-16 02:54:45 +0000

    Update test_city.py

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -34,5 +34,6 @@ class TestCity(unittest.TestCase):
             self.assertTrue(hasattr(self.city, attr_name))
             self.assertIsInstance(getattr(self.city, attr_name), attr_type)
 
+
 if __name__ == "__main__":
     unittest.main()
